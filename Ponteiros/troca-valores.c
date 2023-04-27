f04faf2f204c8cc3129906b43d5d4c2c1d6c52fc
IsisAriele <isisariele1@gmail.com>
2023-04-27 11:20:42 -0300

    Atualização - lista de ponteiros

--- Ponteiros/troca-valores.c
+++ Ponteiros/troca-valores.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
-void swap (int *a, int *b) {
-    int aux;
-    aux = *a;
-    *a = *b;
-    *b = aux;
+void swap(int *a, int *b) {
+    int aux = *a;  // Armazenando o valor apontado por 'a' na variável auxiliar 'aux'
+    *a = *b;       // Atribuindo o valor apontado por 'b' para o endereço apontado por 'a'
+    *b = aux;      // Atribuindo o valor armazenado em 'aux' para o endereço apontado por 'b'
+
 }
 
 int main() {
     int a = 10;
     int b = 20;
 
-    printf("a,b = %d, %d\n", a, b);
+    printf("a, b = %d, %d\n", a, b);
 
-    swap(&a, &b); // Chama a função swap passando os endereços de memória de 'a' e 'b'
+    swap(&a, &b);
 
-    printf("a,b = %d, %d\n", a, b);
+    printf("a, b = %d, %d\n", a, b);
+    
     return 0;
 }
